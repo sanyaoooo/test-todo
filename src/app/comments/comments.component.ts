776d8c1224fe6bceb9c8d743a0e84678a56cb77d
sanyaoooo <sanyaobox@gmail.com>
2022-03-21 11:50:31 +0800

    Fix comments get

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -16,7 +16,14 @@ export class CommentsComponent {
     this.getComments()
   }
   getComments(){
-    this.http.get('https://gorest.co.in/public/v2/posts/100/comments').subscribe({
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 2c2bad61784dd029169ec34b9542429ed4a123f064f7c12e806cccd99c5aac40'
+    });
+    let options = {
+      headers
+    };
+    this.http.get('https://gorest.co.in/public/v2/posts/100/comments?page=1', options).subscribe({
         next: data => {
             this.allcomments = data;
             console.log(this.allcomments)
@@ -45,7 +52,7 @@ export class CommentsComponent {
     let formData = this.commentForm.value
     this.http.post<any>('https://gorest.co.in/public/v2/posts/100/comments', formData, options).subscribe({
       next: data => {
-        this.allcomments = data;
+        this.getComments();
         console.log(data)
       },
       error: error => {
@@ -53,7 +60,7 @@ export class CommentsComponent {
         console.error('There was an error!', error);
       }
     });
-    // this.commentForm.reset()
+    this.commentForm.reset()
   }
 
 }
